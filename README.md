3f958bd2664c5f03c9ad4cd4177fe50c7dab3474
sam <samuel.j.peter@gmail.com>
2024-09-17 00:03:21 +0200

    add docs

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # dagger-poc
 minimal setup to deploy helm charts to k3s cluster using dagger.io
+
+## How-to-Use
+1. install dagger (v0.13.0): `cd /usr/local && curl -fsSL https://dl.dagger.io/dagger/install.sh | DAGGER_VERSION=0.13.0 sudo -E sh`
+2. run `dagger call platform terminal`
+3. once the terminal is ready: `kubectl get services`
--- dagger/src/main/__init__.py
+++ dagger/src/main/__init__.py
@@ -1,16 +1,24 @@
+import asyncio
 import dagger
 from dagger import dag, function
 
 
 @function
-def deployer(config: dagger.File) -> dagger.Container:
-	return (
-		dag.container()
-		.from_("alpine/helm")
-		.with_exec(args=["apk", "add", "kubectl"])
-		.with_env_variable("KUBECONFIG", "/.kube/config")
-		.with_file("/.kube/config", config)
-	)
+async def platform() -> dagger.Container:
+	# Start a k3s server and get the kubeconfig
+	k3s = dag.k3_s("monet")
+	await k3s.server().start()
+	kubce_config = k3s.config()
+	# Deploy some services using helm
+	tasks = [
+		helm_install(kubce_config, "nginx", "18.1.14", "oci://registry-1.docker.io/bitnamicharts"),
+		helm_install(kubce_config, "rabbitmq", "14.7.0", "oci://registry-1.docker.io/bitnamicharts"),
+		helm_install(kubce_config, "postgresql", "15.5.31", "oci://registry-1.docker.io/bitnamicharts")
+		#helm_install(kube_config, "mp-model-platform-test", "0.0.0", "oci://host.docker.internal:5001/helm")
+	]
+	await asyncio.gather(*tasks)
+	# Return a container ready to interact with the cluster
+	return deployer(kubce_config)
 
 @function
 def helm_install(config: dagger.File, chart: str, version: str, repo: str) -> dagger.Container:
@@ -21,11 +29,11 @@ def helm_install(config: dagger.File, chart: str, version: str, repo: str) -> da
 	)
 
 @function
-async def monet_platform() -> dagger.Container:
-	k3s = dag.k3_s("monet")
-	await k3s.server().start()
-	kubce_config = k3s.config()
-	#await helm_install(kube_config, "mp-model-platform-test", "0.0.0", "oci://host.docker.internal:5001/helm")
-	await helm_install(kubce_config, "nginx", "18.1.14", "oci://registry-1.docker.io/bitnamicharts")
-	await helm_install(kubce_config, "rabbitmq", "14.7.0", "oci://registry-1.docker.io/bitnamicharts")
-	return deployer(kubce_config)
+def deployer(config: dagger.File) -> dagger.Container:
+	return (
+		dag.container()
+		.from_("alpine/helm")
+		.with_exec(args=["apk", "add", "kubectl"])
+		.with_env_variable("KUBECONFIG", "/.kube/config")
+		.with_file("/.kube/config", config)
+	)
